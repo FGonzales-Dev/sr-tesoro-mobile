236743f7b4f45c03314801c4aafcc7750d44b153
Francis Gonzales <francisgonzales.private@gmail.com>
2025-09-24 13:40:46 +0800

    added app icon and change name

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,5 +1,6 @@
 class ApiConstants {
-  static const String baseUrl = 'http://localhost:8080/api';
+  // static const String baseUrl = 'http://localhost:8080/api';
+  static const String baseUrl = 'https://tesoro-staging-4458c.ondigitalocean.app/api';
   static const String login = '$baseUrl/auth/login/';
   static const String consultations = '$baseUrl/patients/me/consultations/';
   static const String ecgRecords = '$baseUrl/patients/me/ecg-records/';
